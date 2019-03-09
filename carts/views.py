695c4cac1e38730be7f3679d4be8fe58b82f6b33
tanayparadkar <tanayparadkar@gmail.com>
2019-03-10 00:09:49 +0530

    Cart model

--- carts/views.py
+++ carts/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render 
 
 def cart_home(request):
-    return render(request, "carts/home.html", {})   
+    cart_id = request.session.get("cart_id", None)
+    if cart_id is None: # and isinstance(cart_id, int):
+        print('create new cart')
+        request.session['cart_id'] = 12
+    else:
+        print('Cart ID exists')
+    return render(request, "carts/home.html", {}) 
