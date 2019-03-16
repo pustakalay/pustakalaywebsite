1f90fb587d0e2e7ebe7211f03fe681c714a014c1
tanayparadkar <tanayparadkar@gmail.com>
2019-03-16 12:58:31 +0530

    Order and Billing profile manager

--- billing/models.py
+++ billing/models.py
@@ -1,11 +1,31 @@
 from django.conf import settings
 from django.db import models
 from django.db.models.signals import post_save
+from accounts.models import GuestEmail
 
 User = settings.AUTH_USER_MODEL
 
 # guest abc@teamcfe.com -->> 1000000 billing profiles
 # user abc@teamcfe.com -- 1 billing profile
+
+class BillingProfileManager(models.Manager):
+    def new_or_get(self, request):
+        user = request.user
+        guest_email_id = request.session.get('guest_email_id')
+        created = False
+        obj = None
+        if user.is_authenticated():
+            'logged in user checkout; remember payment stuff'
+            obj, created = self.model.objects.get_or_create(
+                            user=user, email=user.email)
+        elif guest_email_id is not None:
+            'guest user checkout; auto reloads payment stuff'
+            guest_email_obj = GuestEmail.objects.get(id=guest_email_id)
+            obj, created = self.model.objects.get_or_create(
+                                            email=guest_email_obj.email)
+        else:
+            pass
+        return obj, created
 
 class BillingProfile(models.Model):
     user        = models.OneToOneField(User, null=True, blank=True)
@@ -17,6 +37,8 @@ class BillingProfile(models.Model):
 
     def __str__(self):
         return self.email
+    
+    objects = BillingProfileManager()
 
 # def billing_profile_created_receiver(sender, instance, created, *args, **kwargs):
 #     if created:
--- orders/models.py
+++ orders/models.py
@@ -12,6 +12,22 @@ ORDER_STATUS_CHOICES = (
     ('refunded', 'Refunded'),
 )
 
+class OrderManager(models.Manager):
+    def new_or_get(self, billing_profile, cart_obj):
+        created = False
+        qs = self.get_queryset().filter(
+                billing_profile=billing_profile, 
+                cart=cart_obj, 
+                active=True)
+        if qs.count() == 1:
+            obj = qs.first()
+        else:
+            obj = self.model.objects.create(
+                    billing_profile=billing_profile, 
+                    cart=cart_obj)
+            created = True
+        return obj, created
+
 class Order(models.Model):
     billing_profile = models.ForeignKey(BillingProfile, null=True, blank=True)
     order_id        = models.CharField(max_length=120, blank=True) # AB31DE3
@@ -26,6 +42,8 @@ class Order(models.Model):
     
     def __str__(self):
         return self.order_id
+    
+    objects = OrderManager()
     
     def update_total(self):
         cart_total = self.cart.total
