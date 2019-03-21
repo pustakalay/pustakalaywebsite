1add9754132b4dcb8d11483846cec2fdb72cbae7
tanayparadkar <tanayparadkar@gmail.com>
2019-03-21 22:57:41 +0530

    Ajaxify contact form

--- pustakalaywebsite/views.py
+++ pustakalaywebsite/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import ContactForm
+from django.http import HttpResponse, JsonResponse
 
 def home_page(request):
     context = {
@@ -18,5 +19,12 @@ def contact_page(request):
     }
     if contact_form.is_valid():
         print(contact_form.cleaned_data)
+        if request.is_ajax():
+            return JsonResponse({"message": "Thank you for your submission"})
+
+    if contact_form.errors:
+        errors = contact_form.errors.as_json()
+        if request.is_ajax():
+            return HttpResponse(errors, status=400, content_type='application/json')
     return render(request, "contact/view.html", context)
 
