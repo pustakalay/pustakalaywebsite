c1f9de0efaf7c5b79c032b69c6a41eaa960facaa
tanayparadkar <tanayparadkar@gmail.com>
2019-04-18 17:26:23 +0530

    Completing contact us page. Added email confirmation.

--- pustakalaywebsite/views.py
+++ pustakalaywebsite/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from .forms import ContactForm
 from django.http import HttpResponse, JsonResponse
 from django.contrib import messages
+from django.core.mail import EmailMessage
+from django.template.loader import get_template
+from django.conf import settings
 
 def home_page(request):
     context = {
@@ -17,7 +20,27 @@ def contact_page(request):
         "form": contact_form
     }
     if contact_form.is_valid():
-        print(contact_form.cleaned_data)
+        name = contact_form.cleaned_data.get('fullname')
+        email = contact_form.cleaned_data.get('email')
+        content = contact_form.cleaned_data.get('content')
+        context = {
+            'name': name,
+            'content': content
+        }
+        txt_ = get_template("contact/contact-us.txt").render(context)
+#         html_ = get_template("contact/contact-us.html").render(context)
+        subject = 'Thank you for contacting Pustakalay'
+        from_email = settings.DEFAULT_FROM_EMAIL
+        bcc = [settings.DEFAULT_FROM_EMAIL]
+        recipient_list = [email]
+        email_message = EmailMessage(
+                    subject,
+                    txt_,
+                    from_email,
+                    recipient_list,
+                    bcc
+            )        
+        email_message.send(fail_silently=False)
         messages.success(request, "Thank you for your submission.")
         if request.is_ajax():
             return JsonResponse({"message": "Thank you for your submission."})
