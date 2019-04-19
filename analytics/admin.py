4cc7f09931cff3d48154cb7a2ed5e4a84c4068c9
tanayparadkar <tanayparadkar@gmail.com>
2019-04-19 17:44:02 +0530

    Force logout with user session.

--- analytics/admin.py
+++ analytics/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
-
 from .models import ObjectViewed, UserSession
 
+def end_session(modeladmin, request, queryset):
+    for user_session in queryset:
+        user_session.end_session()
+        
+end_session.short_description = "Kill user session"
+
+class UserSessionAdmin(admin.ModelAdmin):
+    list_display = ['user', 'session_key', 'active', 'ended', 'timestamp',]
+    ordering = ['-timestamp']
+    actions = [end_session]
+
 admin.site.register(ObjectViewed)
-admin.site.register(UserSession)
+admin.site.register(UserSession, UserSessionAdmin)
+
